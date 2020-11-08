21d30c0efa2287a88b2007f2d08a5539f9ce95a5
Eugene Theriault <hybridbassist@gmail.com>
2020-11-08 13:59:52 -0800

    Add test for outings relationship to contestants

--- spec/models/outings_spec.rb
+++ spec/models/outings_spec.rb
@@ -6,4 +6,8 @@ describe Outing, type: :model do
     it { should validate_presence_of :location }
     it { should validate_presence_of :date }
   end
+
+  describe 'relationships' do
+    it { should have_many(:contestants).through(:contestant_outings) }
+  end
 end
