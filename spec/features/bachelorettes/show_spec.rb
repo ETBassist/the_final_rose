07de33fddee7cf494959871661958bb5e7672c9f
Eugene Theriault <hybridbassist@gmail.com>
2020-11-08 13:40:06 -0800

    Update tests and views with season description

--- spec/features/bachelorettes/show_spec.rb
+++ spec/features/bachelorettes/show_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 describe "When I visit a bachelorette show page" do
   before :each do
     @bachelorette1 = Bachelorette.create!(name: "Hannah Brown",
-                                         season_number: 5)
+                                         season_number: 5,
+                                         season_description: "EVEN MORE DRAMA")
     @bachelorette2 = Bachelorette.create!(name: "Jane Doe",
-                                         season_number: 8)
+                                         season_number: 8,
+                                         season_description: "WHEN WILL IT END")
     @contestant1 = @bachelorette1.contestants.create(name: "Dwight Shrute",
                                                      age: 34,
                                                      hometown: "Scranton")
@@ -21,7 +23,7 @@ describe "When I visit a bachelorette show page" do
     visit "/bachelorettes/#{@bachelorette1.id}"
 
     expect(page).to have_content(@bachelorette1.name)
-    expect(page).to have_content("Season #{@bachelorette1.season_number} - EVEN MORE DRAMA")
+    expect(page).to have_content("Season #{@bachelorette1.season_number} - #{@bachelorette1.season_description}")
   end
 
   it "I see a link to each contestant for that bachelorette" do
