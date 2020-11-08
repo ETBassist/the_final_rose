61a52197fcc977ad77f23959e85c3a94389de876
Eugene Theriault <hybridbassist@gmail.com>
2020-11-08 13:44:19 -0800

    Create migration and test for validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_213556) do
+ActiveRecord::Schema.define(version: 2020_11_08_214129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2020_11_08_213556) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bachelorette_id"], name: "index_contestants_on_bachelorette_id"
+  end
+
+  create_table "outings", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "date"
   end
 
   add_foreign_key "contestants", "bachelorettes"
