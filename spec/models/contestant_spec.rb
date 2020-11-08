73b40010c8300cf5a4e3dad0d4ca0052b213f45e
Eugene Theriault <hybridbassist@gmail.com>
2020-11-08 14:33:53 -0800

    Create test for average_age method on contestant model

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe Contestant, type: :model do
     it {should belong_to :bachelorette}
     it {should have_many(:outings).through(:contestant_outings)}
   end
+
+  describe 'class methods' do
+    it '::average_age' do
+    bachelorette1 = Bachelorette.create!(name: "Hannah Brown",
+                                         season_number: 5,
+                                         season_description: "EVEN MORE DRAMA")
+    bachelorette2 = Bachelorette.create!(name: "Jane Doe",
+                                         season_number: 8,
+                                         season_description: "WHEN WILL IT END")
+    bachelorette1.contestants.create(name: "Dwight Shrute",
+                                    age: 34,
+                                    hometown: "Scranton")
+    bachelorette1.contestants.create(name: "Michael Scott",
+                                    age: 30,
+                                    hometown: "Scranton")
+    bachelorette2.contestants.create(name: "Jim Halpert",
+                                    age: 29,
+                                    hometown: "Scranton")
+    expect(Contestant.average_age).to eq(31.0)
+
+    end
+  end
 end
