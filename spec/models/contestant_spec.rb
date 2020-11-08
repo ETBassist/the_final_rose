f1bfabfede10439d98689d3e13740292aaec7efc
Eugene Theriault <hybridbassist@gmail.com>
2020-11-08 14:39:43 -0800

    Create test for unique_hometowns method on Contestant model

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -8,23 +8,41 @@ RSpec.describe Contestant, type: :model do
 
   describe 'class methods' do
     it '::average_age' do
-    bachelorette1 = Bachelorette.create!(name: "Hannah Brown",
-                                         season_number: 5,
-                                         season_description: "EVEN MORE DRAMA")
-    bachelorette2 = Bachelorette.create!(name: "Jane Doe",
-                                         season_number: 8,
-                                         season_description: "WHEN WILL IT END")
-    bachelorette1.contestants.create(name: "Dwight Shrute",
-                                    age: 34,
-                                    hometown: "Scranton")
-    bachelorette1.contestants.create(name: "Michael Scott",
-                                    age: 30,
-                                    hometown: "Scranton")
-    bachelorette2.contestants.create(name: "Jim Halpert",
-                                    age: 29,
-                                    hometown: "Scranton")
-    expect(Contestant.average_age).to eq(31.0)
+      bachelorette1 = Bachelorette.create!(name: "Hannah Brown",
+                                          season_number: 5,
+                                          season_description: "EVEN MORE DRAMA")
+      bachelorette2 = Bachelorette.create!(name: "Jane Doe",
+                                          season_number: 8,
+                                          season_description: "WHEN WILL IT END")
+      bachelorette1.contestants.create(name: "Dwight Shrute",
+                                      age: 34,
+                                      hometown: "Scranton")
+      bachelorette1.contestants.create(name: "Michael Scott",
+                                      age: 30,
+                                      hometown: "Scranton")
+      bachelorette2.contestants.create(name: "Jim Halpert",
+                                      age: 29,
+                                      hometown: "Scranton")
+      expect(Contestant.average_age).to eq(31.0)
+    end
 
+    it '::unique_hometowns' do
+      bachelorette1 = Bachelorette.create!(name: "Hannah Brown",
+                                          season_number: 5,
+                                          season_description: "EVEN MORE DRAMA")
+      bachelorette2 = Bachelorette.create!(name: "Jane Doe",
+                                          season_number: 8,
+                                          season_description: "WHEN WILL IT END")
+      contestant1 = bachelorette1.contestants.create(name: "Dwight Shrute",
+                                      age: 34,
+                                      hometown: "Scranton")
+      contestant2 = bachelorette1.contestants.create(name: "Michael Scott",
+                                      age: 30,
+                                      hometown: "Smallville")
+      bachelorette2.contestants.create(name: "Jim Halpert",
+                                      age: 29,
+                                      hometown: "Scranton")
+      expect(Contestant.unique_hometowns).to eq([contestant1.hometown, contestant2.hometown])
     end
   end
 end
